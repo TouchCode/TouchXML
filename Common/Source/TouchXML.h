122d5f00b604c94a0dc21154e9e923ba39ca3ce9
jwight <jwight@43e68342-e247-0410-8716-5592636d597e>
2008-07-22 01:56:17 +0000

    (split)Fix for #18. Missing file. My bad!
    git-svn-id: http://touchcode.googlecode.com/svn/trunk@99 43e68342-e247-0410-8716-5592636d597e
    
    committer: jwight <jwight@43e68342-e247-0410-8716-5592636d597e>
    
    --HG--
    extra : convert_revision : 0be297794dd50390952e4c8423318ceee172e45c

---
+++ Common/Source/TouchXML.h
@@ -0,0 +1,12 @@
+//
+//  TouchXML.h
+//  TouchXML
+//
+//  Created by Jonathan Wight on 07/11/08.
+//  Copyright 2008 Toxic Software. All rights reserved.
+//
+
+#import "CXMLDocument.h"
+#import "CXMLElement.h"
+#import "CXMLNode.h"
+#import "CXMLNode_XPathExtensions.h"
